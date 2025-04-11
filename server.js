72ea9ab755db388761aab51c51beda712946c3fb
Kim beopgi <kimbeopgi562599Ggmail.com>
2025-04-11 19:21:58 +0900

    MySQL 연결 수정

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // MySQL 연결 설정
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "mysql-container",
   user: "root",
   password: "5625",
   database: "marine_db"
